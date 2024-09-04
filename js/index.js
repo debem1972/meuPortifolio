2669bb8a4dee99419324e6bee51a4c2754ba6bb1
Daniel Lopes Bemfica <debem1972@gmail.com>
2024-09-04 16:19:58 -0300

    Foi ajustada a aparição suave da logo após a animação; Foi criado o efeito de sombra abaixo da logo, como sendo um objeto 3d flutuante

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const introText = document.getElementById('intro-text');
     introText.textContent = '<p>Oi! Muito prazer! Eu sou o Daniel! Seja bem vindo ao meu portifólio!</p>';
     setTimeout(() => {
-        //introText.innerHTML = '&lt;/&gt;';  //Tag de fechamento </>
         setTimeout(() => {
             introText.style.display = 'none';
-            document.querySelector('.logo').style.display = 'block';
-        }, 4000);  // Tempo de exibição do '&lt;/&gt;' antes de ocultar o texto e mostrar a logo.
+            const logo = document.querySelector('.logo');
+            logo.style.display = 'block';
+            setTimeout(() => {
+                logo.style.opacity = '1';  // Aplica a transição de opacidade
+            }, 100);  // Um pequeno delay para garantir que a transição seja aplicada
+        }, 4000);  // Tempo de exibição antes de ocultar o texto e mostrar a logo.
     }, 9000); //Tempo total da animação da digitação
 });
